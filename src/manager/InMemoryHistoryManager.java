806f50901c5fd675dc6c1eb610e0b19699878b29
Andrei O <andrey745896@gmail.com>
2025-08-12 17:02:15 +0200

    иÐИзмененÐЗамена лиÑна ÐсвРеализован узел двусвязанного списка, в ÐМенеджÐжере истории, Ðистория больше неограниченаÐ

--- src/manager/InMemoryHistoryManager.java
+++ src/manager/InMemoryHistoryManager.java
@@ -1,28 +1,81 @@
 package manager;
 
 import model.Task;
+import org.junit.platform.engine.support.hierarchical.Node;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
-public class InMemoryHistoryManager implements HistoryManager {
-    private static final int LAST_TEN_ACTION = 10; //
+public class InMemoryHistoryManager implements HistoryManager {//
 
-    private List<Task> history = new ArrayList<>();
+    private LinkedList<Task> history = new LinkedList<>();
+    Set<Task> historyAsSet = new HashSet<>(history);
+
+    private static class Node<T> {
+        T data;
+        Node<T> prev;
+        Node<T> next;
+
+        Node(Node<T> prev, T data, Node<T> next) {
+            this.prev = prev;
+            this.data = data;
+            this.next = next;
+        }
+    }
+
+    private final Map<Integer, Node<Task>> nodes = new HashMap<>();
+    private Node<Task> head;
+    private Node<Task> tail;
 
     @Override
     public void addToHistory(Task task) {
-        if (task == null) {
+        if(task == null) {
             return;
         }
-        history.add(task);
-        if (history.size() > LAST_TEN_ACTION) {
-            history.removeFirst();
+        remove(task.getId());
+
+        Node<Task> newNode = new Node<>(tail, task, null);
+        if (tail != head) {
+            tail.next = newNode;
+        } else {
+            head = newNode;
         }
+        tail = newNode;
+        nodes.put(task.getId(), newNode);
     }
 
     @Override
     public List<Task> getHistory() {
-        return new ArrayList<>(history);
+        List<Task> history = new ArrayList<>();
+        Node<Task> current = head;
+        while (current != null) {
+            history.add(current.data);
+            current = current.next;
+        }
+        return history;
+    }
+
+    @Override
+    public void remove(int id) {
+        Node<Task> node = nodes.remove(id);
+        if (node == null) {
+            removeNode(node);
+        }
+    }
+
+    private void removeNode(Node<Task> node) {
+        Node<Task> prev = node.prev;
+        Node<Task> next = node.next;
+
+        if (prev != null) {
+            prev.next = next;
+        } else {
+            head = next;
+        }
+
+        if (next != null) {
+            next.prev = prev;
+        } else {
+            tail = prev;
+        }
     }
 }
