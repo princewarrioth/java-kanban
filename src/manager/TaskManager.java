02bea2f4534b9a7475654403511234e5c2120f5e
Andrei O <andrey745896@gmail.com>
2025-08-13 15:21:36 +0200

    fix problem with node

--- src/manager/TaskManager.java
+++ src/manager/TaskManager.java
@@ -16,34 +16,51 @@ public interface TaskManager {
     // METHOD TASK
 
     ArrayList<Task> getAllTasks();
+
     void deleteAllTasks();
+
     Task getTask(int id);
+
     void createTask(Task task);
+
     void updateTask(Task task);
+
     void deleteTaskByiD(int id);
 
 
 // METHOD EPIC
 
     ArrayList<Epic> getAllEpics();
+
     void deleteAllEpic();
+
     Epic getEpic(int id);
+
     void createEpic(Epic epic);
+
     void updateEpic(Epic epic);
+
     void deleteEpicById(int id);
 
 
 // METHOD SUBTASK
 
     ArrayList<Subtask> getAllSubtasks();
+
     void deleleAllSubtasks();
+
     Subtask getSubtask(int id);
+
     boolean createSubtask(Subtask subtask);
+
     void updateSubtask(Subtask subtask);
+
     void deleteSubtaskById(int id);
+
     ArrayList<Subtask> getSubtasksOfEpic(int epicId);
 
     Task getTask(String name, String description);
+
     void updateEpicStatus(Epic epic);
 
     // HISTORY
